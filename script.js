fe2a3fb746184451fc80afa6d025aaac7c4a6fe2
Ritik kotgiriwar <87945415+Kotgiriwarritik@users.noreply.github.com>
2023-12-10 22:11:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function myclick() {
+	let firstname = document.getElementById('Name').value;
+	let lastname = document.getElementById('Last').value;
+	let phonenum = document.getElementById('phone').value;
+	let emailid = document.getElementById('email').value;
+
+	let result = "First Name: "+firstname+"\n"
+	             +"Last Name: "+lastname+"\n"
+	             +"Phone Number: "+phonenum+"\n"
+	             +"Email ID: "+emailid;
+
+	alert(result);
+	
+}
