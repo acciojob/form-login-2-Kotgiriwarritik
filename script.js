3a1c9345bd855378d36eb3aaf5956cc3471f210b
Ritik kotgiriwar <87945415+Kotgiriwarritik@users.noreply.github.com>
2023-12-18 14:26:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 //your JS code here. If required.
-function myclick() {
-	let firstname = document.getElementById('Name').value;
-	let lastname = document.getElementById('Last').value;
-	let phonenum = document.getElementById('phone').value;
-	let emailid = document.getElementById('email').value;
+ function displayResults() {
+            var firstName = document.getElementById("firstName").value;
+            var lastName = document.getElementById("lastName").value;
+            var phoneNumber = document.getElementById("phoneNumber").value;
+            var email = document.getElementById("email").value;
 
-	let result = "First Name: "+firstname+"\n"
-	             +"Last Name: "+lastname+"\n"
-	             +"Phone Number: "+phonenum+"\n"
-	             +"Email ID: "+emailid;
+            var result = "First Name: " + firstName + "\n" +
+                         "Last Name: " + lastName + "\n" +
+                         "Phone Number: " + phoneNumber + "\n" +
+                         "Email ID: " + email;
 
-	alert(result);
-	
-}
+            alert(result);
+        }
